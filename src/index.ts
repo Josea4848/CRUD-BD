8bbce3a8aba4bf03b237fdc73986c0205e9b77a0
Josea4848 <josealves3416@gmail.com>
2024-08-23 10:13:01 -0300

    routes: add and delte clients

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import exp = require("constants");
-import * as cors from "cors";
 import * as express from "express"; //framework para API
 import { AppDataSource } from "./data-source";
 import { Car } from "./entity/Car";
 import { Client } from "./entity/Client";
 import { Sale } from "./entity/Sale";
-import { getAllCars } from "./methods/car_methods";
-import { addClient, getClient } from "./methods/client_methods";
+import { addCar, getAllCars, getAvailableCars } from "./methods/car_methods";
+import * as cors from "cors";
+import { addSaleIdCPF } from "./methods/sale_methods";
+import {
+  addClient,
+  getAllClients,
+  getClient,
+  removeClientCPF,
+} from "./methods/client_methods";
 
 // server.js
 const express = require("express");
@@ -25,10 +31,27 @@ AppDataSource.initialize()
     const car_table = AppDataSource.getRepository(Car);
     const sale_table = AppDataSource.getRepository(Sale);
 
+    addClient("22233344488", "João", "Alves", "2004/03/04", client_table);
+    addClient("22233345488", "Akita", "Fabinho", "1995/03/03", client_table);
+    addClient("22232344488", "João", "Alves", "2004/03/04", client_table);
+    addClient("22231345488", "Akita", "Fabinho", "1995/03/03", client_table);
+    addClient("22433344488", "João", "Alves", "2004/03/04", client_table);
+    addClient("22833345488", "Akita", "Fabinho", "1995/03/03", client_table);
+    addClient("22033344488", "João", "Alves", "2004/03/04", client_table);
+    addClient("20033345488", "Akita", "Fabinho", "1995/03/03", client_table);
+    addClient("00033344488", "João", "Alves", "2004/03/04", client_table);
+    addClient("22233345478", "Akita", "Fabinho", "1995/03/03", client_table);
+    addClient("22233344477", "João", "Alves", "2004/03/04", client_table);
+    addClient("22233345466", "Akita", "Fabinho", "1995/03/03", client_table);
+    addClient("22233344444", "João", "Alves", "2004/03/04", client_table);
+    addClient("22233345411", "Akita", "Fabinho", "1995/03/03", client_table);
+    addClient("22233344455", "João", "Alves", "2004/03/04", client_table);
+    addClient("22233345433", "Akita", "Fabinho", "1995/03/03", client_table);
+
     //GET
     app.get("/cars", async (req, res) => {
       try {
-        return res.status(200).json(await getAllCars(car_table));
+        return res.status(200).json(await getAvailableCars(car_table));
       } catch (error) {
         return res.status(500).send(error.message);
       }
@@ -46,24 +69,70 @@ AppDataSource.initialize()
     });
 
     //SELL CAR BY ID -> INSERT INTO SALE TABLE AND UPDATE CAR STATUS
-    app.post("/sell/:id", async (req, res) => {
+    app.post("/sell", async (req, res) => {
       //INSERT
       try {
+        const data = await req.body;
+        console.log(`Venda recebida: ${data}`);
+        await addSaleIdCPF(
+          data.cpf,
+          data.car_id,
+          2000,
+          client_table,
+          car_table,
+          sale_table
+        );
+        res.status(201).json(data);
       } catch (error) {
         return res.status(500).send(error.message);
       }
     });
 
-    //Clients
-    app.get("/clients/:id", async (req, res) => {
+    // ------------------------- Clients --------------------------------------
+
+    //create clients
+    app.post("/clients", async (req, res) => {
       try {
-        const client = await getClient(req.params.id, client_table);
+        const data = await req.body;
+        console.log(`Solitição de registro de cliente ${data}`);
+        res.status(200).json(data);
+      } catch (error) {
+        return res.status(500).send(error.message);
+      }
+    });
+
+    //Get clients
+    app.get("/clients", async (req, res) => {
+      try {
+        const clients = await getAllClients(client_table);
+        console.log(clients);
+        return res.status(200).json(clients);
+      } catch (error) {
+        return res.status(500).send(error.message);
+      }
+    });
+
+    //get clients by id
+    app.get("/clients/:cpf", async (req, res) => {
+      try {
+        const client = await getClient(req.params.cpf, client_table);
         let is_client = true;
 
         if (client == null) {
           is_client = false;
         }
         return res.status(200).json({ isClient: is_client });
+      } catch (error) {
+        return res.status(500).send(error.message);
+      }
+    });
+
+    //delete client by cpf
+    app.delete("/clients/:cpf", async (req, res) => {
+      try {
+        const client = await removeClientCPF(req.params.cpf, client_table);
+        console.log(`Client (CPF: ${req.params.cpf}) was deleted`);
+        res.status(200).json(client);
       } catch (error) {
         return res.status(500).send(error.message);
       }
--- src/methods/client_methods.ts
+++ src/methods/client_methods.ts
@@ -2,7 +2,13 @@ import { Repository } from "typeorm";
 import { Client } from "../entity/Client";
 
 //----------------------------------------- CREATE ---------------------------------------------
-export async function addClient(CPF: string, first_name: string, last_name: string, birthdate: string, client_table: Repository<Client>): Promise<Client>{
+export async function addClient(
+  CPF: string,
+  first_name: string,
+  last_name: string,
+  birthdate: string,
+  client_table: Repository<Client>
+): Promise<Client> {
   if (isCPFValid(CPF)) {
     const client = new Client(CPF, first_name, last_name, birthdate);
     await client_table.save(client);
@@ -12,71 +18,101 @@ export async function addClient(CPF: string, first_name: string, last_name: stri
 }
 
 //------------------------------------------ READ ---------------------------------------------
-export async function getAllClients(client_table: Repository<Client>): Promise<Client[]>{
-  return await client_table.find()
+export async function getAllClients(
+  client_table: Repository<Client>
+): Promise<Client[]> {
+  return await client_table.find();
 }
 
-export async function getClient(CPF: string, client_table: Repository<Client>): Promise<Client>{
+export async function getClient(
+  CPF: string,
+  client_table: Repository<Client>
+): Promise<Client> {
   if (isCPFValid(CPF)) {
     return await client_table.findOneBy({ CPF: CPF });
   }
   return;
 }
 
-export async function getClientByName(first_name: string, last_name: string, client_table: Repository<Client>): Promise<Client[]>{
-  if(first_name == null){
-    return await client_table.findBy({last_name: last_name})
-  }else if(last_name == null){
-    return await client_table.findBy({first_name: first_name})
-  }else{
-    const clients = await client_table.findBy({first_name: first_name, last_name: last_name})
+export async function getClientByName(
+  first_name: string,
+  last_name: string,
+  client_table: Repository<Client>
+): Promise<Client[]> {
+  if (first_name == null) {
+    return await client_table.findBy({ last_name: last_name });
+  } else if (last_name == null) {
+    return await client_table.findBy({ first_name: first_name });
+  } else {
+    const clients = await client_table.findBy({
+      first_name: first_name,
+      last_name: last_name,
+    });
     if (clients == null) {
-      return await client_table.findBy({ first_name: first_name })
-    } else{
-      return clients
+      return await client_table.findBy({ first_name: first_name });
+    } else {
+      return clients;
     }
   }
 }
 
 //--------------------------------------- UPDATE ---------------------------------------------
-export async function updateClientName(CPF: string, new_first_name: string, new_last_name: string, client_table: Repository<Client>): Promise<void>{
-  if(new_first_name == null){
-    await client_table.update(CPF, {last_name: new_last_name})
-  }else if(new_last_name == null){
-    await client_table.update(CPF, {first_name: new_first_name})
-  }else{
-    await client_table.update(CPF, {first_name: new_first_name, last_name: new_last_name})
+export async function updateClientName(
+  CPF: string,
+  new_first_name: string,
+  new_last_name: string,
+  client_table: Repository<Client>
+): Promise<void> {
+  if (new_first_name == null) {
+    await client_table.update(CPF, { last_name: new_last_name });
+  } else if (new_last_name == null) {
+    await client_table.update(CPF, { first_name: new_first_name });
+  } else {
+    await client_table.update(CPF, {
+      first_name: new_first_name,
+      last_name: new_last_name,
+    });
   }
 }
 
-export async function updateClientBirthdate(CPF:string, date: string, client_table: Repository<Client>): Promise<void> {
-  if(isCPFValid(CPF)){
-    await client_table.update(CPF, {birthdate: date})
+export async function updateClientBirthdate(
+  CPF: string,
+  date: string,
+  client_table: Repository<Client>
+): Promise<void> {
+  if (isCPFValid(CPF)) {
+    await client_table.update(CPF, { birthdate: date });
   }
 }
 
 //--------------------------------------- DELETE ---------------------------------------------
-export async function removeClient(client: Client, client_table: Repository<Client>): Promise<void>{
-  await client_table.remove(client)
+export async function removeClient(
+  client: Client,
+  client_table: Repository<Client>
+): Promise<void> {
+  await client_table.remove(client);
 }
 
-export async function removeClientCPF(CPF: string, client_table: Repository<Client>): Promise<void> {
+export async function removeClientCPF(
+  CPF: string,
+  client_table: Repository<Client>
+): Promise<void> {
   if (isCPFValid(CPF)) {
     const client = await getClient(CPF, client_table);
-    await client_table.remove(client)
+    await client_table.remove(client);
   }
 }
 
 //--------------------------------------- checks --------------------------------------------
-function isCPFValid(CPF: string): boolean{
-  if(CPF.length != 11){
+function isCPFValid(CPF: string): boolean {
+  if (CPF.length != 11) {
     throw new Error("Not a valid CPF");
     return false;
   }
 
-  if(!(/^\d{11}$/.test(CPF))){
+  if (!/^\d{11}$/.test(CPF)) {
     throw new Error("Not a valid CPF");
     return false;
-  };
+  }
   return true;
 }
